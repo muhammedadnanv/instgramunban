9973905db7bee085fba38e3ade7e7a963d63e4d7
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-05 15:41:58 +0530

    Create backup.js

---
+++ backup.js
@@ -0,0 +1,7 @@
+const backupInformation = (data) => {
+  // Implement the backup logic here
+  console.log(`Backing up information: ${data}`);
+};
+
+const dataToBackup = ' textdata';
+backupInformation(dataToBackup);
